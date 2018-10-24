3dd7764df98eb3355e2bb4dd1ee4f40e8331ae17
Andrii Vasyliev <sol@hiqdev.com>
2018-10-24 16:37:24 +0000

    Added exceptions

---
+++ src/ExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiapi\directi;
+
+/**
+ * Common interface for all exceptions thrown by this library.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+interface ExceptionInterface
+{
+}
